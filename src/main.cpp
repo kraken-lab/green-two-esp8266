85af9a125d0b5b29939a20878ec6519118ec8e97
ahermann4 <ahermann4@gmx.de>
2021-04-25 10:23:20 +0200

    check littlefs content

--- src/main.cpp
+++ src/main.cpp
@@ -3,202 +3,49 @@
   Complete project details at http://randomnerdtutorials.com  
 *********/
 
-// Load Wi-Fi library
-#include <ESP8266WiFi.h>
+#include <Arduino.h>
+#include "LittleFS.h"
 
-#include "SoftwareSerial.h"
-#include "serial.h"
-
-// Replace with your network credentials
-const char* ssid     = "WLAN-BEF106_EXT";
-const char* password = "cocacola";
-
-// Set web server port number to 80
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-
-// Auxiliar variables to store the current output state
-String output5State = "off";
-String output4State = "off";
-
-// Assign output variables to GPIO pins
-const int output5 = 5;
-const int output4 = 4;
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
-// Rx and Tx pins
-const int _serialRx = 3;
-const int _serialTx = 1;
-
-// pins for serial communication
-SoftwareSerial s(_serialRx,_serialTx);
-
-// data from arduino
-int _tempInteger = 1;
-int _tempFraction = 1;
-int _humidity = 0;
-
-// Set your Static IP address
-IPAddress local_IP(192, 168, 2, 184);
-// Set your Gateway IP address
-IPAddress gateway(192, 168, 2, 1);
-
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8);   //optional
-IPAddress secondaryDNS(8, 8, 4, 4); //optional
+bool print = true;
 
 void setup() {
   Serial.begin(115200);
-  // serial connection for communication with arduino uno
-  s.begin(9600);
-  // Initialize the output variables as outputs
-  pinMode(output5, OUTPUT);
-  pinMode(output4, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output5, LOW);
-  digitalWrite(output4, LOW);
-
-  // Configures static IP address
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-    Serial.println("STA Failed to configure");
+}
+ 
+void printFileSystemContent() {
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
   }
-
-
-  delay(5000);
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  
+  File file = LittleFS.open("/text.txt", "r");
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+  
+  Serial.println("\n\n\nFile Content:");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+void loop() {  
+  if (print)
+  {
+    delay(1000);
+    Serial.write("1");
+    delay(1000);
+    Serial.write("2");
+    delay(1000);
+    Serial.write("3");
+    delay(1000);
+    Serial.write("4");
+    delay(1000);
+    Serial.write("5");
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, LOW);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6AFF; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A88;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>Green v1.0</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-            // Display getTemperature button
-            client.println(
-              "<p>Temperature: </p>"
-              " "
-              + _tempInteger
-              + _tempFraction);
-            client.println("<p><a href=\"/temp\"><button class=\"button\">get temperature</button></a></p>");
-
-            // Display getHumidity button
-            client.println(
-              "<p>Humidity: </p>"
-              " "
-              + _tempInteger
-              + _tempFraction);
-            client.println("<p><a href=\"/temp\"><button class=\"button\">get temperature</button></a></p>");
-
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+    printFileSystemContent();
+    print = false;
   }
 }
